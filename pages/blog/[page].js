33ff5236f0662b9ddf78d82808ab3b5dbff7eb76
koalascode <90786950+koalascode@users.noreply.github.com>
2022-05-23 17:04:28 -0400

    added style to the blog posts and added the nav bar to them

--- pages/blog/[page].js
+++ pages/blog/[page].js
@@ -3,7 +3,7 @@ import styles from '/styles/ArticleTemplate.module.css'
 import Image from 'next/image'
 import Link from 'next/link'
 import { useRouter } from 'next/router'
-
+import NavBar from '../../components/navbar'
 
 export default function Article({ allprops, pageprops }) {
     console.log(pageprops.results.map(x => x?.video?.external?.url))
@@ -27,9 +27,8 @@ export default function Article({ allprops, pageprops }) {
         <div>
             <title>{allprops.properties.Name.title[0].plain_text}</title>
             <link rel="icon" href="/blogicon.png"/>
-            <Link href="/blog" passHref>
-                <button className={styles.returnhomebutton}>⬅️ Blog Home</button>
-            </Link>
+            <NavBar />
+            <br />
             <div className={styles.allarticle}>
             <h1 className={styles.title}>{allprops.properties.Name.title[0].plain_text}</h1>
             <div className={styles.headerinfo}>
